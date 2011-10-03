e7141dcd3f305a52b76a37216fd9a051d5f97630
eu <myemail@gmail.com>
2011-10-03 11:46:28 -0300

    Criação do Projeto 'Agenda' - Rails

---
+++ agenda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = 'a61dec814567f01b0cba851f28f4d779ec7147ae6806452568fbd0cb4b66c48c8c9672863f6f3d0b73f34d489ff48c7c7f9a697b37b906974cac73e839975840'
