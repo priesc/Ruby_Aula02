3a2571f86066672e953edae1c2a9452b75e99db8
eu <myemail@gmail.com>
2011-10-03 11:31:38 -0300

    Criação da pasta sinatra_aula02

---
+++ sinatra_aula02/app.rb
@@ -0,0 +1,26 @@
+#encoding: utf-8
+
+require 'sinatra'
+
+get '/:nome/:telefone/:email' do
+	@nome= params[:nome]
+	@telefone= params[:telefone]
+	@email= params[:email]
+	erb :index, layout: :layout #Padrão layout.erb
+end
+
+get '/ola' do
+	erb :ola
+end
+
+get '/teste' do
+	erb :teste
+end
+
+post '/bom-dia' do
+	@nome= params[:nome]
+	@email= params[:email]
+	@telefone= params[:telefone]
+
+	erb :bomdia
+end
